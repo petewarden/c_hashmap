0b95adff96b6af90f573a8bcda66868e4ebf882b
wd0468 <wd0468@175game.com>
2016-06-21 17:55:03 +0800

    playgit

---
+++ a.c
@@ -0,0 +1,25 @@
+
+/* ====================================================
+#   Copyright (C)2016 All rights reserved.
+#
+#   Author        : betterboy
+#   Email         : betterboy3@live.com
+#   File Name     : a.c
+#   Last Modified : 2016-06-21 00:27
+#   Describe      :
+#
+#   Log           :
+#
+# ====================================================*/
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 5;
+	int b = 10;
+	printf("a/b=%f\n", (float)a/b);
+	return 0;
+}
+
+
